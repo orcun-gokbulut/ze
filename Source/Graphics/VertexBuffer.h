e91a4506afc0674f8c114ea8c42fb15ff43890df
Yiğit Orçun Gökbulut <orcun.gokbulut@gmail.com>
2009-08-25 07:11:42 +0000

    svn path=/trunk/; revision=17

--- Source/Graphics/VertexBuffer.h
+++ Source/Graphics/VertexBuffer.h
@@ -77,8 +77,7 @@ class ZEArrayVertexBuffer : public ZEDynamicVertexBuffer
 {
 	public:
 		ZEArray<_VertexType, _Allocator> Vertices;
-
-		virtual 
+ 
 		virtual unsigned int GetBufferSize()
 		{
 			return Vertices.GetCount() * sizeof(_VertexType);
