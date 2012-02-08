f0be0435beb14f7d7d6dcd93a869ed93910f6e80
Gökay Özaktürk <>
2012-02-08 15:20:35 +0000

    ADD: ZED3D9MLAAProcessor implemented. CHG: FarZ changed to 1000.
    svn path=/trunk/; revision=784

---
+++ RunDir/resources/shaders/MLAAProcessor.hlsl
@@ -0,0 +1,328 @@
+//ZE_SOURCE_PROCESSOR_START(License, 1.0)
+/*******************************************************************************
+ Zinek Engine - MLAAProcessor.hlsl
+ ------------------------------------------------------------------------------
+ Copyright (C) 2008-2021 Yiğit Orçun GÖKBULUT. All rights reserved.
+
+ This file is part of the Zinek Engine  Software. Zinek Engine Software and the
+ accompanying  materials are  made available  under the  terms of Zinek  Engine
+ Commercial License or the GNU General Public License Version 3.
+
+                      ZINEK ENGINE COMMERCIAL LICENSE
+ Licensees  holding  valid  Zinek Engine Commercial  License(s) may  use  Zinek
+ Engine  Software in  accordance  with   the  commercial  license  agreement(s)
+ (which can only be  issued  by  copyright  owner "Yiğit  Orçun  GÖKBULUT") and
+ provided with the Software  or, alternatively,  in  accordance with the  terms
+ contained  in  a  written  agreement  between  you  and  copyright  owner. For
+ licensing  terms  and conditions  please  contact  with  copyright owner.
+
+                    GNU GENERAL PUBLIC LICENSE VERSION 3
+ This program is free software: you can  redistribute it and/or modify it under
+ the terms of the GNU General Public  License as published by the Free Software
+ Foundation, either  version 3 of  the License, or  (at your option)  any later
+ version. This program is  distributed in the hope that  it will be useful, but
+ WITHOUT ANY WARRANTY; without even the  implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. See  the GNU General Public License for more
+ details. You  should have received  a copy of the  GNU General  Public License
+ along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+ Copyright Owner Information:
+  Name: Yiğit Orçun GÖKBULUT
+  Contact: orcun.gokbulut@gmail.com
+  Github: https://www.github.com/orcun-gokbulut/ZE
+*******************************************************************************/
+//ZE_SOURCE_PROCESSOR_END()
+
+// Includes
+#include "GBuffer.hlsl"
+
+// Samplers
+sampler2D 	ColorBuffer		: register(s3);
+sampler2D	EdgeBuffer		: register(s4);
+sampler2D	WeightBuffer	: register(s5);
+sampler2D	AreaBuffer		: register(s6);
+
+// Vertex shader parameters
+float2 		PixelSize		: register(vs, c0)
+							: register(ps, c0);
+
+// Pixel shader paramters
+float2		MLAAParameters	: register(ps, c1);
+
+
+#define		Treshold		MLAAParameters.x
+#define		FarZ			MLAAParameters.y
+
+#define		SEARCH_STEP		2
+#define		NUM_DISTANCES	32.0f
+#define		AREA_SIZE (NUM_DISTANCES * 5.0f)
+
+
+/************************************************************************/
+/*					        IO Structures			                    */
+/************************************************************************/
+
+// Vertex Shader
+struct VS_INPUT
+{
+	float3 Position : POSITION0;
+	
+};
+
+struct VS_OUTPUT 
+{
+	float4 Position		: POSITION0;
+	float2 TextureCoord	: TEXCOORD0;
+
+};
+
+// Pixel Shader 
+struct PS_INPUT
+{
+	float2 TextureCoord	: TEXCOORD0;
+	
+};
+
+struct PS_OUTPUT
+{
+	float4 PixelColor : COLOR0;
+
+};
+
+
+/************************************************************************/
+/*                Common Vertex Shader for All Passes	                */
+/************************************************************************/
+
+VS_OUTPUT vs_main_common(VS_INPUT Input)
+{
+	VS_OUTPUT Output = (VS_OUTPUT)0.0f;
+   
+	Output.Position	= float4(sign(Input.Position).xyz, 1.0f);
+	
+	Output.TextureCoord.x = 0.5f * (1.0f + Output.Position.x + PixelSize.x);
+	Output.TextureCoord.y = 0.5f * (1.0f - Output.Position.y + PixelSize.y);
+	
+	return Output;
+}
+
+/************************************************************************/
+/*                   MLAA Shader Helper Functions	                    */
+/************************************************************************/
+
+float4 MultiplyAdd(float4 M, float4 A, float4 B)
+{
+	return M * A + B;
+}
+
+float2 Area(float2 Distance, float E1, float E2)
+{
+	float2 PixelCoord = NUM_DISTANCES * round(4.0f * float2(E1, E2)) + Distance;
+	float2 TextureCoord = PixelCoord / (AREA_SIZE - 1.0f);
+
+	return tex2Dlod(AreaBuffer, float4(TextureCoord.x, TextureCoord.y, 0.0f, 0.0f)).rg;
+}
+
+float SearchLeft(float2 TextureCoord)
+{
+	float E = 0.0f;
+	TextureCoord -= float2(1.5f, 0.0f) * PixelSize;
+
+	int I = 0;
+	[unroll(SEARCH_STEP)]
+    while (I < SEARCH_STEP)
+	{
+		E = tex2Dlod(EdgeBuffer, float4(TextureCoord.x, TextureCoord.y, 0.0f, 0.0f)).g;
+		[flatten] if (E < 0.9f) break;
+		TextureCoord -= float2(2.0f, 0.0f) * PixelSize;
+		I++;
+    }
+    
+    return max(-2.0f * I - 2.0f * E, -2.0f * SEARCH_STEP);
+}
+
+float SearchRight(float2 TextureCoord)
+{
+	float E = 0.0f;
+	TextureCoord += float2(1.5f, 0.0f) * PixelSize;
+
+	int I = 0;
+	[unroll(SEARCH_STEP)]
+    while (I < SEARCH_STEP)
+	{
+		E = tex2Dlod(EdgeBuffer, float4(TextureCoord.x, TextureCoord.y, 0.0f, 0.0f)).g;
+		[flatten] if (E < 0.9f) break;
+		TextureCoord += float2(2.0f, 0.0f) * PixelSize;
+		I++;
+    }
+    
+    return min(2.0f * I + 2.0f * E, 2.0f * SEARCH_STEP);
+}
+
+float SearchUp(float2 TextureCoord)
+{
+	TextureCoord -= float2(0.0f, 1.5f) * PixelSize;
+	float E = 0.0f;
+
+	int I = 0;
+	[unroll(SEARCH_STEP)]
+    while (I < SEARCH_STEP)
+	{
+		E = tex2Dlod(EdgeBuffer, float4(TextureCoord.x, TextureCoord.y, 0.0f, 0.0f)).r;
+		[flatten] if (E < 0.9f) break;
+		TextureCoord -= float2(0.0f, 2.0f) * PixelSize;
+		I++;
+    }
+    
+    return max(-2.0f * I - 2.0f * E, -2.0f * SEARCH_STEP);
+}
+
+float SearchDown(float2 TextureCoord)
+{
+	TextureCoord += float2(0.0f, 1.5f) * PixelSize;
+	float E = 0.0f;
+
+	int I = 0;
+	[unroll(SEARCH_STEP)]
+    while (I < SEARCH_STEP)
+	{
+		E = tex2Dlod(EdgeBuffer, float4(TextureCoord.x, TextureCoord.y, 0.0f, 0.0f)).r;
+		[flatten] if (E < 0.9f) break;
+		TextureCoord += float2(2.0f, 0.0f) * PixelSize;
+		I++;
+    }
+    
+    return min(2.0f * I + 2.0f * E, 2.0f * SEARCH_STEP);
+}
+
+/************************************************************************/
+/*              Pixel Shader for Edge Detection                         */
+/************************************************************************/
+
+PS_OUTPUT ps_main_edge_detection_depth(PS_INPUT Input)
+{
+	PS_OUTPUT Output = (PS_OUTPUT)0.0f;
+
+	// Get Depths
+	float PixelDepthCenter	= (float4)1.0f - ZEGBuffer_GetDepth(Input.TextureCoord) / FarZ;
+	float PixelDepthUp		= ZEGBuffer_GetDepth(Input.TextureCoord - float2(0.0f, PixelSize.y));
+	float PixelDepthDown	= ZEGBuffer_GetDepth(Input.TextureCoord + float2(0.0f, PixelSize.y));
+	float PixelDepthLeft	= ZEGBuffer_GetDepth(Input.TextureCoord - float2(PixelSize.x, 0.0f));
+	float PixelDepthRight	= ZEGBuffer_GetDepth(Input.TextureCoord + float2(PixelSize.x, 0.0f));
+	
+	float4 Depths = (float4)1.0f - float4(PixelDepthLeft, PixelDepthUp, PixelDepthRight, PixelDepthDown) / FarZ;
+	float4 DepthGradient = abs(PixelDepthCenter.rrrr - Depths);
+
+	// Get Normals
+	float3 PixelNormCenter = ZEGBuffer_GetViewNormal(Input.TextureCoord);
+
+	float3 PixelNormUp		= ZEGBuffer_GetViewNormal(Input.TextureCoord - float2(0.0f, PixelSize.y));
+	float3 PixelNormDown	= ZEGBuffer_GetViewNormal(Input.TextureCoord + float2(0.0f, PixelSize.y));
+	float3 PixelNormLeft	= ZEGBuffer_GetViewNormal(Input.TextureCoord - float2(PixelSize.x, 0.0f));
+	float3 PixelNormRight	= ZEGBuffer_GetViewNormal(Input.TextureCoord + float2(PixelSize.x, 0.0f));
+
+	float4 AngleGradient = (float4)0.0f;
+	AngleGradient.x = dot(PixelNormCenter, PixelNormLeft);
+	AngleGradient.y = dot(PixelNormCenter, PixelNormUp);
+	AngleGradient.z = dot(PixelNormCenter, PixelNormRight);
+	AngleGradient.w = dot(PixelNormCenter, PixelNormDown);
+	
+	// Distance based angle treshold
+	float AngleTreshold = (1.0f - PixelDepthCenter) * 0.7f + 0.05f;
+	
+	// Reverse map 0-180 degrees[-1 to +1] to 0-1
+	AngleGradient = 1.0f - (AngleGradient * 0.5f + 0.5f);
+		
+	float4 Angles = step(AngleTreshold.xxxx, AngleGradient);
+	float4 Depth = step(Treshold.xxxx / 10.0f, DepthGradient);
+	
+	// Combine angle and depth edge detection results
+	Output.PixelColor = (Angles < 1.0f) ? 0.0f : 1.0f; 
+	Output.PixelColor = (Depth < 1.0f) ? Output.PixelColor : 1.0f;
+	
+	// Output.PixelColor = (Angles + Depth) * 0.5f;
+	// Output.PixelColor = step(float4(0.4f, 0.4f, 0.4f, 0.4f), Output.PixelColor);
+
+
+	if (dot(Output.PixelColor, 1.0f) == 0.0f) discard;
+
+	Output.PixelColor.b = 0.0f;
+	Output.PixelColor.a = 0.0f;
+	return Output;
+}
+
+/************************************************************************/
+/*                 Pixel Shader for Weight Blending	                    */
+/************************************************************************/
+
+PS_OUTPUT ps_main_weight_blending(PS_INPUT Input)
+{
+	PS_OUTPUT Output = (PS_OUTPUT)0.0f;
+
+	float2 E = tex2Dlod(EdgeBuffer, float4(Input.TextureCoord.x, Input.TextureCoord.y, 0.0f, 0.0f)).rg;
+
+	[branch]
+    if (E.g) // Edge at north
+	{
+        float2 D = float2(SearchLeft(Input.TextureCoord), SearchRight(Input.TextureCoord));
+        
+        float4 Coords = MultiplyAdd(float4(D.x, -0.25f, D.y + 1.0f, -0.25f), PixelSize.xyxy, Input.TextureCoord.xyxy);
+        float E1 = tex2Dlod(EdgeBuffer, float4(Coords.x, Coords.y, 0.0f, 0.0f)).r;
+        float E2 = tex2Dlod(EdgeBuffer, float4(Coords.z, Coords.w, 0.0f, 0.0f)).r;
+        
+		Output.PixelColor.rg = Area(abs(D), E1, E2);
+    }
+
+	[branch]
+    if (E.r) // Edge at west
+	{
+        float2 D = float2(SearchUp(Input.TextureCoord), SearchDown(Input.TextureCoord));
+
+        float4 Coords = MultiplyAdd(float4(-0.25, D.x, -0.25, D.y + 1.0), PixelSize.xyxy, Input.TextureCoord.xyxy);
+		float E1 = tex2Dlod(EdgeBuffer, float4(Coords.x, Coords.y, 0.0f, 0.0f)).g;
+        float E2 = tex2Dlod(EdgeBuffer, float4(Coords.z, Coords.w, 0.0f, 0.0f)).g;
+        
+		Output.PixelColor.ba = Area(abs(D), E1, E2);
+    }
+
+	return Output;
+}
+
+/************************************************************************/
+/*                Pixel Shader for Color Blending                       */
+/************************************************************************/
+PS_OUTPUT ps_main_color_blending(PS_INPUT Input)
+{
+	PS_OUTPUT Output = (PS_OUTPUT)0.0f;
+	
+	float4 UpLeft	= tex2D(WeightBuffer, Input.TextureCoord);
+	float  Right	= tex2D(WeightBuffer, Input.TextureCoord + float2(0.0f, PixelSize.y)).g;
+	float  Down		= tex2D(WeightBuffer, Input.TextureCoord + float2(PixelSize.x, 0.0f)).a;
+	
+	float4 A		= float4(UpLeft.r, Right, UpLeft.b, Down);
+	float Sum	= dot(A, float4(1.0f, 1.0f, 1.0f, 1.0f));
+
+	float4 TexCoord = float4(Input.TextureCoord.x, Input.TextureCoord.y, 0.0f, 0.0f);
+
+	[branch]
+    if (Sum > 0.0f) 
+	{
+        float4 O = A * PixelSize.yyxx;
+     
+        Output.PixelColor = MultiplyAdd(tex2Dlod(ColorBuffer, TexCoord + float4( 0.0f, -O.r , 0.0f, 0.0f)), A.r, Output.PixelColor);
+        Output.PixelColor = MultiplyAdd(tex2Dlod(ColorBuffer, TexCoord + float4( 0.0f,  O.g , 0.0f, 0.0f)), A.g, Output.PixelColor);
+        Output.PixelColor = MultiplyAdd(tex2Dlod(ColorBuffer, TexCoord + float4(-O.b ,  0.0f, 0.0f, 0.0f)), A.b, Output.PixelColor);
+        Output.PixelColor = MultiplyAdd(tex2Dlod(ColorBuffer, TexCoord + float4( O.a ,  0.0f, 0.0f, 0.0f)), A.a, Output.PixelColor);
+        
+		Output.PixelColor /= Sum;
+    }
+	else 
+	{
+        Output.PixelColor = tex2Dlod(ColorBuffer, TexCoord);
+    }
+
+	return Output;
+}
+
+
+
--- Source/ZEEngine/ZEGraphics/ZEGraphicsDebugModule.cpp
+++ Source/ZEEngine/ZEGraphics/ZEGraphicsDebugModule.cpp
@@ -33,45 +33,75 @@
 *******************************************************************************/
 //ZE_SOURCE_PROCESSOR_END()
 
+
+#include "ZEBitmap.h"
+#include "ZEGame/ZEGrid.h"
 #include "ZEGame/ZEGame.h"
 #include "ZEGame/ZEScene.h"
+#include "ZEMath/ZEAngle.h"
+#include "ZEModel/ZEModel.h"
 #include "ZEGame/ZEPlayer.h"
+#include "ZEUI/ZEUIManager.h"
+#include "ZEGame/ZESkyDome.h"
+#include "ZESkyDomeMaterial.h"
 #include "ZEGame/ZESkyBrush.h"
-#include "ZEGame/ZECanvasBrush.h"
-#include "ZEGame/ZEEntityProvider.h"
+#include "ZEUI/ZEFontResource.h"
 #include "ZEGraphics/ZECamera.h"
+#include "ZEUI/ZEUITextControl.h"
+#include "ZEGame/ZECanvasBrush.h"
 #include "ZEGraphicsDebugModule.h"
+#include "ZEGame/ZEEntityProvider.h"
 #include "ZEGraphics/ZEPointLight.h"
 #include "ZEGraphics/ZESimpleMaterial.h"
 #include "ZEGraphics/ZEGraphicsModule.h"
 #include "ZEGraphics/ZEProjectiveLight.h"
 #include "ZEGraphics/ZEProjectiveLight.h"
 #include "ZEGraphics/ZEDirectionalLight.h"
+#include "ZEMap/ZEPortalMap/ZEPortalMap.h"
 #include "ZEGraphics/ZETexture2DResource.h"
 #include "ZEGraphics/ZEOmniProjectiveLight.h"
 #include "ZEGraphics/ZETextureCubeResource.h"
-#include "ZEMath/ZEAngle.h"
-#include "ZEMap/ZEPortalMap/ZEPortalMap.h"
-#include "ZEModel/ZEModel.h"
-#include "ZEUI/ZEFontResource.h"
-#include "ZEUI/ZEUIManager.h"
+
 
 #include <stdlib.h>
-#include "ZEGame/ZESkyDome.h"
-#include "ZEUI/ZEUITextControl.h"
-#include "ZEGame/ZEGrid.h"
-#include "ZESkyDomeMaterial.h"
-
+#include <malloc.h>
+#include <stdio.h>
 
 
 bool ZEGraphicsDebugModule::Initialize()
 {
+	// Load "AreaMap32.tga"
+	/*
+	unsigned char* Buffer = (unsigned char*)malloc(sizeof(unsigned char) * 4 * 160 * 160);
+	ZEBitmap Bibibitmap;
+	Bibibitmap.Load("resources\\AreaMap32.tga");
+	Bibibitmap.CopyTo(Buffer, 160 * 4, 160, 160);
+
+	FILE* thefile = fopen("Color.txt", "a+");
+	
+
+	for (int i=0; i<160; i++)
+	{
+		for (int j=0; j<160; j++)
+		{
+			unsigned char* Pixel = Buffer + 160 * 4 * i + j * 4;
+			fprintf(thefile, "%3d, %3d, %3d, %3d, ", (int)*(Pixel + 0), (int)*(Pixel + 1), (int)*(Pixel + 2), (int)*(Pixel + 3));
+			
+			if (j % 39 == 0 && j != 0)
+				fprintf(thefile, "\n");
+
+			fflush(thefile);
+		}
+		
+	}
+	*/
+
 	ZEScene* Scene = zeGame->GetScene();
 	
 	ZEPortalMap* Map = ZEPortalMap::CreateInstance();
-	Map->SetMapFile("Ground.ZEMAP");
+	Map->SetMapFile("deneme.ZEMAP");
 	Map->SetPosition(ZEVector3(0.0f, 0.0f, 0.0f));
-	Map->SetName("Ground.ZEMAP");
+	Map->SetName("deneme.ZEMAP");
 	Scene->AddEntity(Map);
 	
 	Player = ZEPlayer::CreateInstance();
@@ -81,7 +111,7 @@ bool ZEGraphicsDebugModule::Initialize()
 	Player->GetCamera()->SetNearZ(zeGraphics->GetNearZ());
 	Player->GetCamera()->SetFarZ(zeGraphics->GetFarZ());
 	Player->GetCamera()->SetFOV(ZE_PI / 3.0f);
-	Player->SetMovementSpeed(500.0f);
+	Player->SetMovementSpeed(10.0f);
 	Scene->SetActiveCamera(Player->GetCamera());
 	Scene->AddEntity(Player);
 
@@ -115,11 +145,83 @@ bool ZEGraphicsDebugModule::Initialize()
 	MovementSpeed->SetFont(Font);
 	zeGame->UIManager->AddControl(MovementSpeed);
 
+	PointLight1 = ZEPointLight::CreateInstance();
+	PointLight1->SetPosition(ZEVector3(-6.0f, 3.0f, -2.0f));
+	PointLight1->SetColor(ZEVector3(1.0f, 1.0f, 1.0f));
+	PointLight1->SetAttenuation(0.01f, 0.0f, 1.0f);
+	PointLight1->SetIntensity(1.0f);
+	PointLight1->SetRange(55.0f);
+	PointLight1->SetCastShadows(false);
+	PointLight1->SetVisible(true);
+	PointLight1->SetEnabled(true);
+	PointLight1->SetName("TestLight1");
+	Scene->AddEntity(PointLight1);
+
+	PointLight2 = ZEPointLight::CreateInstance();
+	PointLight2->SetPosition(ZEVector3(-15.0f, 3.0f, -15.0f));
+	PointLight2->SetColor(ZEVector3(1.0f, 1.0f, 1.0f));
+	PointLight2->SetAttenuation(0.01f, 0.0f, 1.0f);
+	PointLight2->SetIntensity(1.0f);
+	PointLight2->SetRange(55.0f);
+	PointLight2->SetCastShadows(false);
+	PointLight2->SetVisible(true);
+	PointLight2->SetEnabled(true);
+	PointLight2->SetName("TestLight2");
+	Scene->AddEntity(PointLight2);
+
+	PointLight3 = ZEPointLight::CreateInstance();
+	PointLight3->SetPosition(ZEVector3(-33.0f, 3.0f, -1.0f));
+	PointLight3->SetColor(ZEVector3(1.0f, 1.0f, 1.0f));
+	PointLight3->SetAttenuation(0.01f, 0.0f, 1.0f);
+	PointLight3->SetIntensity(1.0f);
+	PointLight3->SetRange(55.0f);
+	PointLight3->SetCastShadows(false);
+	PointLight3->SetVisible(true);
+	PointLight3->SetEnabled(true);
+	PointLight3->SetName("TestLight3");
+	Scene->AddEntity(PointLight3);
+
+	PointLight4 = ZEPointLight::CreateInstance();
+	PointLight4->SetPosition(ZEVector3(-17.0f, 4.0f, 37.0f));
+	PointLight4->SetColor(ZEVector3(1.0f, 1.0f, 1.0f));
+	PointLight4->SetAttenuation(0.01f, 0.0f, 1.0f);
+	PointLight4->SetIntensity(1.0f);
+	PointLight4->SetRange(55.0f);
+	PointLight4->SetCastShadows(false);
+	PointLight4->SetVisible(true);
+	PointLight4->SetEnabled(true);
+	PointLight4->SetName("TestLight4");
+	Scene->AddEntity(PointLight4);
+
+	PointLight5 = ZEPointLight::CreateInstance();
+	PointLight5->SetPosition(ZEVector3(-32.0f, 3.0f, 24.0f));
+	PointLight5->SetColor(ZEVector3(1.0f, 1.0f, 1.0f));
+	PointLight5->SetAttenuation(0.01f, 0.0f, 1.0f);
+	PointLight5->SetIntensity(1.0f);
+	PointLight5->SetRange(55.0f);
+	PointLight5->SetCastShadows(false);
+	PointLight5->SetVisible(true);
+	PointLight5->SetEnabled(true);
+	PointLight5->SetName("TestLight5");
+	Scene->AddEntity(PointLight5);
+
+	PointLight6 = ZEPointLight::CreateInstance();
+	PointLight6->SetPosition(ZEVector3(-50.0f, 15.0f, -10.0f));
+	PointLight6->SetColor(ZEVector3(1.0f, 1.0f, 1.0f));
+	PointLight6->SetAttenuation(0.01f, 0.0f, 1.0f);
+	PointLight6->SetIntensity(1.0f);
+	PointLight6->SetRange(55.0f);
+	PointLight6->SetCastShadows(false);
+	PointLight6->SetVisible(true);
+	PointLight6->SetEnabled(true);
+	PointLight6->SetName("TestLight6");
+	Scene->AddEntity(PointLight6);
+
 	DirectionalLight0 = ZEDirectionalLight::CreateInstance();
 	DirectionalLight0->SetEnabled(true);
 	DirectionalLight0->SetRotation(ZEQuaternion(ZE_PI, ZEVector3::UnitY));
 	DirectionalLight0->SetColor(ZEVector3(1.0f, 1.0f, 1.0f));
-	DirectionalLight0->SetIntensity(2.0f);
+	DirectionalLight0->SetIntensity(1.0f);
 	DirectionalLight0->SetCastsShadow(false);
 	DirectionalLight0->SetVisible(true);
 	DirectionalLight0->SetName("TestDirectionalLight0");
@@ -134,8 +236,8 @@ bool ZEGraphicsDebugModule::Initialize()
 	SkyDome->SetInnerRadius(60000.0f);
 	// For camera on the ground case, it is advised to use offset as (0.0f, InnerRadius, 0.0f) 
 	SkyDome->SetCameraPositionOffset(ZEVector3(0.0f, 60000.0f, 0.0f));
-	Scene->AddEntity(SkyDome);
-
+	//Scene->AddEntity(SkyDome);
+	
 	// Scene->AddEntity(ZEGrid::CreateInstance());
 
 	return true;
@@ -143,7 +245,6 @@ bool ZEGraphicsDebugModule::Initialize()
 
 void ZEGraphicsDebugModule::Deinitialize()
 {
-
 	zeScene->SetActiveCamera(NULL);
 
 	Player->Destroy();
@@ -188,15 +289,6 @@ void ZEGraphicsDebugModule::Process(float ElapsedTime)
 
 	sprintf(buffer3, "Movement Speed: %.3lf", ((ZEPlayer*)(zeScene->GetActiveCamera()->GetOwner()))->GetMovementSpeed());
 	MovementSpeed->SetText((ZEString)buffer3);
-
-	// P = point on the circle
-	// t = parameter of the parametric equation [0-1]
-	// u = unit direction vector to sun
-	// R = radius
-	// n = unit vector perpendicular to circle plane
-	// c = center of th circle
-	// P = R * cos(t) * u + R * sin(t) * n * u + c
-
 
 	static float RotX = 0.0f;
 	static float RotY = 0.0f;
