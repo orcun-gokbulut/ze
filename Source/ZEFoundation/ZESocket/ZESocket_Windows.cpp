96dba91271febc6e0859cae800bddd050a5ab8e7
Can Bilgen <bilgencan@yahoo.com>
2012-06-05 07:34:18 +0000

    CHG : Too much log was caused by TCPListener's accept function. Log mechanism commented out.
    svn path=/trunk/; revision=926

--- Source/ZEFoundation/ZESocket/ZESocket_Windows.cpp
+++ Source/ZEFoundation/ZESocket/ZESocket_Windows.cpp
@@ -1,6 +1,6 @@
 //ZE_SOURCE_PROCESSOR_START(License, 1.0)
 /*******************************************************************************
- Zinek Engine - ZESocketWinSock.cpp
+ Zinek Engine - ZESocket_Windows.cpp
  ------------------------------------------------------------------------------
  Copyright (C) 2008-2021 Yiğit Orçun GÖKBULUT. All rights reserved.
 
@@ -379,7 +379,7 @@ ZESocketTCP* ZESocketTCPListener::Accept()
 
 	if(Result == INVALID_SOCKET)
 	{
-		zeLog("Accepted socket is not valid, WinSock Error code : %d", WSAGetLastError());
+		//zeLog("Accepted socket is not valid, WinSock Error code : %d", WSAGetLastError());
 		return NULL;
 	}
 
