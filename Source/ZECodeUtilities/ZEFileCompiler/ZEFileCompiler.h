a41d19e73182292f58095186072e9b698e257927
Egemen Barutçu <ege155@gmail.com>
2012-12-15 09:20:50 +0000

    ADD: ZECodeTools have been renamed to ZECodeUtilities. Due to SVN bug renaming will be done in several steps.
    svn path=/branches/v0.5.5-NewTools/; revision=1168

--- Source/ZECodeUtilities/ZEFileCompiler/ZEFileCompiler.h
+++ Source/ZECodeUtilities/ZEFileCompiler/ZEFileCompiler.h
@@ -1,6 +1,6 @@
-#ZE_SOURCE_PROCESSOR_START(License, 1.0)
-#[[*****************************************************************************
- Zinek Engine - CMakeLists.txt
+//ZE_SOURCE_PROCESSOR_START(License, 1.0)
+/*******************************************************************************
+ Zinek Engine - ZEFileCompiler.h
  ------------------------------------------------------------------------------
  Copyright (C) 2008-2021 Yiğit Orçun GÖKBULUT. All rights reserved.
 
@@ -30,16 +30,20 @@
   Name: Yiğit Orçun GÖKBULUT
   Contact: orcun.gokbulut@gmail.com
   Github: https://www.github.com/orcun-gokbulut/ZE
-*****************************************************************************]]
-#ZE_SOURCE_PROCESSOR_END()
+*******************************************************************************/
+//ZE_SOURCE_PROCESSOR_END()
 
-cmake_minimum_required(VERSION 2.8)
+#pragma once
+#ifndef __ZE_FILE_COMPILER_H__
+#define __ZE_FILE_COMPILER_H__
 
-ze_add_module(ZECodeUtilities)
-ze_add_module(ZEFoundation)
-ze_add_module(ZEEngine)
-ze_add_module(ZEModules			OPTIONAL DEFAULT)
-ze_add_module(ZEditor			OPTIONAL DEFAULT)
-ze_add_module(ZETools			OPTIONAL DEFAULT)
+#include "ZEDS/ZEString.h"
+#include "ZEFileCompilerOptions.h"
 
-ze_add_cmake_project(ZESource)
+class ZEFileCompiler
+{
+	public:
+		static bool		Compile(const ZEFileCompilerOptions& Options);
+};
+
+#endif
