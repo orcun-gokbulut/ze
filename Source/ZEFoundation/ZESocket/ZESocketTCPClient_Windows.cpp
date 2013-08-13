8287a65b32a9949868c643633e0c9daba4875cbb
Yiğit Orçun Gökkbulut <orcun.gokbulut@gmail.com>
2013-08-13 08:08:23 +0000

    FIX: Missing ZESocket files has been committed.
    svn path=/trunk/; revision=1510

---
+++ Source/ZEFoundation/ZESocket/ZESocketTCPClient_Windows.cpp
@@ -0,0 +1,178 @@
+//ZE_SOURCE_PROCESSOR_START(License, 1.0)
+/*******************************************************************************
+ Zinek Engine - ZESocketTCPClient_Windows.cpp
+ ------------------------------------------------------------------------------
+ Copyright (C) 2008-2021 Yiğit Orçun GÖKBULUT. All rights reserved.
+
+ This file is part of the Zinek Engine  Software. Zinek Engine Software and the
+ accompanying  materials are  made available  under the  terms of Zinek  Engine
+ Commercial License or the GNU General Public License Version 3.
+
+                      ZINEK ENGINE COMMERCIAL LICENSE
+ Licensees  holding  valid  Zinek Engine Commercial  License(s) may  use  Zinek
+ Engine  Software in  accordance  with   the  commercial  license  agreement(s)
+ (which can only be  issued  by  copyright  owner "Yiğit  Orçun  GÖKBULUT") and
+ provided with the Software  or, alternatively,  in  accordance with the  terms
+ contained  in  a  written  agreement  between  you  and  copyright  owner. For
+ licensing  terms  and conditions  please  contact  with  copyright owner.
+
+                    GNU GENERAL PUBLIC LICENSE VERSION 3
+ This program is free software: you can  redistribute it and/or modify it under
+ the terms of the GNU General Public  License as published by the Free Software
+ Foundation, either  version 3 of  the License, or  (at your option)  any later
+ version. This program is  distributed in the hope that  it will be useful, but
+ WITHOUT ANY WARRANTY; without even the  implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. See  the GNU General Public License for more
+ details. You  should have received  a copy of the  GNU General  Public License
+ along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+ Copyright Owner Information:
+  Name: Yiğit Orçun GÖKBULUT
+  Contact: orcun.gokbulut@gmail.com
+  Github: https://www.github.com/orcun-gokbulut/ZE
+*******************************************************************************/
+//ZE_SOURCE_PROCESSOR_END()
+
+#include "ZESocketTCPClient.h"
+
+#include <Winsock2.h>
+
+void ZESocketTCPClient::UpdateStatus()
+{
+	if (Status == ZE_STCS_CONNECTING)
+	{
+		sockaddr_in Info;
+		ZEIPAddress::ToSockaddr_in(&Info, ServerIPAddress);
+		Info.sin_port = htons(Port);
+
+		int Result = connect(Socket, (SOCKADDR*)&Info, sizeof(sockaddr_in));
+		if (Result == 0)
+			Status = ZE_STCS_CONNECTED;
+		else if (Result != EALREADY)
+			Close();
+	}
+}
+
+ZESocketTCPClientStatus ZESocketTCPClient::GetStatus()
+{
+	UpdateStatus();
+	return Status;
+}
+
+bool ZESocketTCPClient::Open()
+{
+	if (Status != ZE_STCS_DISCONNECTED)
+		return true;
+
+	Socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if(Socket == INVALID_SOCKET)
+	{
+		zeError("Can not create TCP Socket, WinSock Error code : %d", WSAGetLastError());
+		return false;
+	}
+
+	if (BindIPAddress != ZEIPAddress::Any)
+	{
+		sockaddr_in LocalInfo;
+		ZEIPAddress::ToSockaddr_in(&LocalInfo, BindIPAddress);
+		LocalInfo.sin_port = htons(Port);
+
+		if(bind(Socket, (SOCKADDR*)&LocalInfo, sizeof(LocalInfo)) == SOCKET_ERROR)
+		{
+			zeError("Can not bind IPv4 TCP Socket, WinSock Error code : %d", WSAGetLastError());
+			Close();
+			return false;
+		}
+	}
+
+	u_long IMode = 1;
+	if(ioctlsocket(Socket, FIONBIO, &IMode) == SOCKET_ERROR)
+	{
+		zeError("Can not set TCP Socket non-blocking mode, WinSock Error code : %d", WSAGetLastError());
+		Close();
+		return false;
+	}
+
+	sockaddr_in ServerInfo;
+	ZEIPAddress::ToSockaddr_in(&ServerInfo, ServerIPAddress);
+	ServerInfo.sin_port = htons(Port);
+	if(connect(Socket, (SOCKADDR*)&ServerInfo, sizeof(ServerInfo)) == SOCKET_ERROR)
+	{
+		ZEInt Error = WSAGetLastError();
+		if(Error == WSAEWOULDBLOCK || Error == WSAEALREADY)
+		{
+			Status = ZE_STCS_CONNECTING;
+			return true;
+		}
+		else if(Error == WSAEISCONN)
+		{
+			Status = ZE_STCS_CONNECTED;
+			return true;
+		}
+		else
+		{	
+			Status = ZE_STCS_DISCONNECTED;
+			Close();
+		}	
+	}
+
+	Status = ZE_STCS_CONNECTED;
+	return true;
+}
+
+void ZESocketTCPClient::Close()
+{
+	if (Socket != NULL)
+		return;
+
+	closesocket(Socket);
+	Socket = NULL;
+
+	Status = ZE_STCS_DISCONNECTED;
+}
+
+ZESSize ZESocketTCPClient::Send(const void* Buffer, ZESize BufferSize)
+{
+	if (GetStatus() != ZE_STCS_CONNECTED)
+		return ZE_SR_ERROR;
+
+	ZESSize Result = send(Socket, (const char*)Buffer, (int)BufferSize, 0);
+	if(Result < 0)
+	{
+		ZEInt Error = WSAGetLastError();
+		if (Error == WSAEWOULDBLOCK)
+		{
+			return ZE_SR_RETRY;
+		}
+		else
+		{
+			Close();
+			return ZE_SR_ERROR;
+		}
+	}
+
+	return Result;
+}
+
+ZESSize ZESocketTCPClient::Receive(void* Buffer, ZESize BufferSize)
+{
+	if (GetStatus() != ZE_STCS_CONNECTED)
+		return ZE_SR_ERROR;
+
+	ZESSize Result = recv(Socket, (char*)Buffer, (int)BufferSize, 0);
+	if(Result < 0)
+	{
+		ZEInt Error = WSAGetLastError();
+		if (Error == WSAEWOULDBLOCK)
+		{
+			return ZE_SR_RETRY;
+		}
+		else
+		{
+			Close();
+			return ZE_SR_ERROR;
+		}
+	}
+
+	return Result;
+}
---
+++ Source/ZEFoundation/ZESocket/ZESocketUDP.cpp
@@ -0,0 +1,92 @@
+//ZE_SOURCE_PROCESSOR_START(License, 1.0)
+/*******************************************************************************
+ Zinek Engine - ZESocketUDP.cpp
+ ------------------------------------------------------------------------------
+ Copyright (C) 2008-2021 Yiğit Orçun GÖKBULUT. All rights reserved.
+
+ This file is part of the Zinek Engine  Software. Zinek Engine Software and the
+ accompanying  materials are  made available  under the  terms of Zinek  Engine
+ Commercial License or the GNU General Public License Version 3.
+
+                      ZINEK ENGINE COMMERCIAL LICENSE
+ Licensees  holding  valid  Zinek Engine Commercial  License(s) may  use  Zinek
+ Engine  Software in  accordance  with   the  commercial  license  agreement(s)
+ (which can only be  issued  by  copyright  owner "Yiğit  Orçun  GÖKBULUT") and
+ provided with the Software  or, alternatively,  in  accordance with the  terms
+ contained  in  a  written  agreement  between  you  and  copyright  owner. For
+ licensing  terms  and conditions  please  contact  with  copyright owner.
+
+                    GNU GENERAL PUBLIC LICENSE VERSION 3
+ This program is free software: you can  redistribute it and/or modify it under
+ the terms of the GNU General Public  License as published by the Free Software
+ Foundation, either  version 3 of  the License, or  (at your option)  any later
+ version. This program is  distributed in the hope that  it will be useful, but
+ WITHOUT ANY WARRANTY; without even the  implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. See  the GNU General Public License for more
+ details. You  should have received  a copy of the  GNU General  Public License
+ along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+ Copyright Owner Information:
+  Name: Yiğit Orçun GÖKBULUT
+  Contact: orcun.gokbulut@gmail.com
+  Github: https://www.github.com/orcun-gokbulut/ZE
+*******************************************************************************/
+//ZE_SOURCE_PROCESSOR_END()
+
+#include "ZESocketUDP.h"
+
+void ZESocketUDP::SetBindIPAddress(const ZEIPAddress& IPAddress)
+{
+	BindIPAddress = IPAddress;
+}
+
+const ZEIPAddress& ZESocketUDP::GetBindIPAddress() const
+{
+	return BindIPAddress;
+}
+
+void ZESocketUDP::SetRemoteIPAddress(const ZEIPAddress& IPAddress)
+{
+	RemoteIPAddress = IPAddress;
+}
+
+const ZEIPAddress& ZESocketUDP::GetRemoteIPAddress() const
+{
+	return RemoteIPAddress;
+}
+
+void ZESocketUDP::SetPort(ZEUInt16 Port)
+{
+	this->Port = Port;
+}
+
+ZEUInt16 ZESocketUDP::GetPort() const
+{
+	return Port;
+}
+
+ZESSize ZESocketUDP::Send(const void* Buffer, ZESize BufferSize)
+{
+	return Send(RemoteIPAddress, Buffer, BufferSize);
+}
+
+ZESSize ZESocketUDP::Receive(void* Buffer, ZESize BufferSize)
+{
+	ZEIPAddress Address;
+	ZESSize Result = Receive(Address, Buffer, BufferSize);
+	if (Address != RemoteIPAddress)
+		return ZE_SR_RETRY;
+}
+
+ZESocketUDP::ZESocketUDP()
+{
+	Socket = NULL;
+	Status = ZE_SUS_CLOSED;
+	BindIPAddress = ZEIPAddress::Any;
+	Port = 0;
+}
+
+ZESocketUDP::~ZESocketUDP()
+{
+	Close();
+}
