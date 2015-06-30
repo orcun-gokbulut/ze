dd76de1069108c290dea40c24b82c8abb4ed023a
Yiğit Orçun Gökkbulut <orcun.gokbulut@gmail.com>
2015-06-30 11:28:43 +0000

    !!! BROKEN BUILD !!! IMP: ZERenderer has been improved.
    svn path=/branches/v0.6.2-Graphics/; revision=1683

---
+++ Source/ZEEngine/ZERenderer/ZERNStageGBuffer.h
@@ -0,0 +1,65 @@
+//ZE_SOURCE_PROCESSOR_START(License, 1.0)
+/*******************************************************************************
+ Zinek Engine - ZERNStageGBuffer.h
+ ------------------------------------------------------------------------------
+ Copyright (C) 2008-2021 Yiğit Orçun GÖKBULUT. All rights reserved.
+
+ This file is part of the Zinek Engine  Software. Zinek Engine Software and the
+ accompanying  materials are  made available  under the  terms of Zinek  Engine
+ Commercial License or the GNU General Public License Version 3.
+
+                      ZINEK ENGINE COMMERCIAL LICENSE
+ Licensees  holding  valid  Zinek Engine Commercial  License(s) may  use  Zinek
+ Engine  Software in  accordance  with   the  commercial  license  agreement(s)
+ (which can only be  issued  by  copyright  owner "Yiğit  Orçun  GÖKBULUT") and
+ provided with the Software  or, alternatively,  in  accordance with the  terms
+ contained  in  a  written  agreement  between  you  and  copyright  owner. For
+ licensing  terms  and conditions  please  contact  with  copyright owner.
+
+                    GNU GENERAL PUBLIC LICENSE VERSION 3
+ This program is free software: you can  redistribute it and/or modify it under
+ the terms of the GNU General Public  License as published by the Free Software
+ Foundation, either  version 3 of  the License, or  (at your option)  any later
+ version. This program is  distributed in the hope that  it will be useful, but
+ WITHOUT ANY WARRANTY; without even the  implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. See  the GNU General Public License for more
+ details. You  should have received  a copy of the  GNU General  Public License
+ along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+ Copyright Owner Information:
+  Name: Yiğit Orçun GÖKBULUT
+  Contact: orcun.gokbulut@gmail.com
+  Github: https://www.github.com/orcun-gokbulut/ZE
+*******************************************************************************/
+//ZE_SOURCE_PROCESSOR_END()
+
+#pragma once
+
+#include "ZERNStage.h"
+#include "ZEGraphics\ZEGRHolder.h"
+
+class ZEGRTexture2D;
+
+class ZERNStageGBuffer : public ZERNStage
+{
+	private:
+		ZEGRHolder<ZEGRTexture2D>		GBuffer0;
+		ZEGRHolder<ZEGRTexture2D>		GBuffer1;
+		ZEGRHolder<ZEGRTexture2D>		GBuffer2;
+		ZEGRHolder<ZEGRTexture2D>		GBuffer3;
+		ZEGRHolder<ZEGRTexture2D>		AccumulationBuffer;
+		ZEGRHolder<ZEGRRenderState>		RenderState;
+
+	public:
+		ZEGRTexture2D*					GetPositionBuffer();
+		ZEGRTexture2D*					GetNormalBuffer();
+		ZEGRTexture2D*					GetDiffuseBuffer();
+		ZEGRTexture2D*					GetSpecularBuffer();
+		ZEGRTexture2D*					GetAccumulationBuffer();
+
+		virtual ZEGRRenderState*		GetRenderState();
+
+		virtual void					Setup(ZEGRDevice* Device);
+		virtual void					CleanUp();
+		virtual void					Reconfigure();
+};
