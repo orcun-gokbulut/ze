78bdc0b98c4b73754037decb9f5d681eec9e6571
Yiğit Orçun Gökkbulut <orcun.gokbulut@gmail.com>
2015-07-31 08:37:49 +0000

    ADD: ZEList replacement ZEList2 has been added. ADD: ZEList2 Visual Studio debug viewers have been added to ZE.natvis.
    svn path=/branches/v0.6.2-Graphics/; revision=1699

--- Source/ZEFoundation/ZEDS/ZEList2.cpp
+++ Source/ZEFoundation/ZEDS/ZEList2.cpp
@@ -1,6 +1,6 @@
 //ZE_SOURCE_PROCESSOR_START(License, 1.0)
 /*******************************************************************************
- Zinek Engine - ZEData.cpp
+ Zinek Engine - ZEList2.cpp
  ------------------------------------------------------------------------------
  Copyright (C) 2008-2021 Yiğit Orçun GÖKBULUT. All rights reserved.
 
@@ -33,3 +33,4 @@
 *******************************************************************************/
 //ZE_SOURCE_PROCESSOR_END()
 
+#include "ZEList2.h"
---
+++ Source/ZEFoundation/ZEDS/ZEList2.h
@@ -0,0 +1,573 @@
+//ZE_SOURCE_PROCESSOR_START(License, 1.0)
+/*******************************************************************************
+ Zinek Engine - ZEList2.h
+ ------------------------------------------------------------------------------
+ Copyright (C) 2008-2021 Yiğit Orçun GÖKBULUT. All rights reserved.
+
+ This file is part of the Zinek Engine  Software. Zinek Engine Software and the
+ accompanying  materials are  made available  under the  terms of Zinek  Engine
+ Commercial License or the GNU General Public License Version 3.
+
+                      ZINEK ENGINE COMMERCIAL LICENSE
+ Licensees  holding  valid  Zinek Engine Commercial  License(s) may  use  Zinek
+ Engine  Software in  accordance  with   the  commercial  license  agreement(s)
+ (which can only be  issued  by  copyright  owner "Yiğit  Orçun  GÖKBULUT") and
+ provided with the Software  or, alternatively,  in  accordance with the  terms
+ contained  in  a  written  agreement  between  you  and  copyright  owner. For
+ licensing  terms  and conditions  please  contact  with  copyright owner.
+
+                    GNU GENERAL PUBLIC LICENSE VERSION 3
+ This program is free software: you can  redistribute it and/or modify it under
+ the terms of the GNU General Public  License as published by the Free Software
+ Foundation, either  version 3 of  the License, or  (at your option)  any later
+ version. This program is  distributed in the hope that  it will be useful, but
+ WITHOUT ANY WARRANTY; without even the  implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. See  the GNU General Public License for more
+ details. You  should have received  a copy of the  GNU General  Public License
+ along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+ Copyright Owner Information:
+  Name: Yiğit Orçun GÖKBULUT
+  Contact: orcun.gokbulut@gmail.com
+  Github: https://www.github.com/orcun-gokbulut/ZE
+*******************************************************************************/
+//ZE_SOURCE_PROCESSOR_END()
+#pragma once
+
+#include "ZECommon.h"
+#include "ZETypes.h"
+#include "ZEError.h"
+
+template<typename ZEItemType> class ZEList2;
+
+template<typename ZEItemType>
+class ZELink
+{
+	ZE_DISALLOW_COPY(ZELink);
+	friend class ZEList2<ZEItemType>;
+	private:
+		ZEList2<ZEItemType>*		List;
+		ZEItemType*					Item;
+		ZELink*						Prev;
+		ZELink*						Next;
+
+	public:
+		ZEList2<ZEItemType>*		GetList();
+		const ZEList2<ZEItemType>*	GetList() const;
+		ZELink*						GetPrev();
+		const ZELink*				GetPrev() const;
+		ZELink*						GetNext();
+		const ZELink*				GetNext() const;
+		ZEItemType*					GetItem() const;
+
+		void						InsertBefore(ZELink<ZEItemType>* Item);
+		void						InsertAfter(ZELink<ZEItemType>* Item);
+		void						Remove();
+
+									ZELink(ZEItemType* Object);
+									~ZELink();
+};
+
+template<typename ZEItemType>
+class ZEList2
+{
+	ZE_DISALLOW_COPY(ZEList2)
+	private:
+		ZELink<ZEItemType>*			First;
+		ZELink<ZEItemType>*			Last;
+		ZESize						Count;
+
+	public:
+		ZESize						GetCount() const;
+
+		ZELink<ZEItemType>*			GetFirst();
+		const ZELink<ZEItemType>*	GetFirst() const;
+		ZELink<ZEItemType>*			GetLast();
+		const ZELink<ZEItemType>*	GetLast() const;
+
+		ZEItemType*					GetItem(ZESize Index);
+		ZELink<ZEItemType>*			GetLink(ZESize Index);
+		const ZELink<ZEItemType>*	GetLink(ZESize Index) const;
+
+		ZELink<ZEItemType>*			Find(const ZEItemType* Item, ZELink<ZEItemType>* StartLink = NULL);
+		const ZELink<ZEItemType>*	Find(const ZEItemType* Item, const ZELink<ZEItemType>* StartLink = NULL) const;
+		ZESize						FindIndex(ZELink<ZEItemType>* Link) const;
+
+		void						AddBegin(ZELink<ZEItemType>* Link);
+		void						AddEnd(ZELink<ZEItemType>* Link);
+		
+		void						InsertAfter(ZELink<ZEItemType>* Link, ZELink<ZEItemType>* NewLink);
+		void						InsertBefore(ZELink<ZEItemType>* Link, ZELink<ZEItemType>* NewLink);
+		
+		void						Remove(ZELink<ZEItemType>* Link);
+		void						Swap(ZELink<ZEItemType>* A, ZELink<ZEItemType>* B);
+
+		void						Reverse();
+
+		template<ZEInt CompareFunction(const ZEItemType*, const ZEItemType*)>
+		void						Sort();
+
+		void						Clean();
+
+		ZEItemType*					operator[](ZESize Index) const;
+
+									ZEList2();
+									~ZEList2();
+};
+
+template<typename ZEItemType>
+ZEList2<ZEItemType>* ZELink<ZEItemType>::GetList()
+{
+	return List;
+}
+
+template<typename ZEItemType>
+const ZEList2<ZEItemType>* ZELink<ZEItemType>::GetList() const
+{
+	return List;
+}
+
+
+template<typename ZEItemType>
+ZELink<ZEItemType>* ZELink<ZEItemType>::GetPrev()
+{
+	return Prev;
+}
+
+template<typename ZEItemType>
+const ZELink<ZEItemType>* ZELink<ZEItemType>::GetPrev() const
+{
+	return Prev;
+}
+
+template<typename ZEItemType>
+ZELink<ZEItemType>* ZELink<ZEItemType>::GetNext()
+{
+	return Next;
+}
+
+template<typename ZEItemType>
+const ZELink<ZEItemType>* ZELink<ZEItemType>::GetNext() const
+{
+	return Next;
+}
+
+template<typename ZEItemType>
+ZEItemType* ZELink<ZEItemType>::GetItem() const
+{
+	return Item;
+}
+
+template<typename ZEItemType>
+void ZELink<ZEItemType>::InsertBefore(ZELink<ZEItemType>* Item)
+{
+	zeDebugCheck(List == NULL, "This links is not contained in any list.");
+
+	List->InsertBefore(this, Item);
+}
+
+template<typename ZEItemType>
+void ZELink<ZEItemType>::InsertAfter(ZELink<ZEItemType>* Item)
+{
+	zeDebugCheck(List == NULL, "This links is not contained in any list.");
+	
+	List->InsertAfter(this, Item);
+}
+
+template<typename ZEItemType>
+void ZELink<ZEItemType>::Remove()
+{
+	zeDebugCheck(List == NULL, "This links is not contained in any list.");
+	List->Remove(this);
+}
+
+template<typename ZEItemType>
+ZELink<ZEItemType>::ZELink(ZEItemType* Object)
+{
+	 List = NULL;
+	 Prev = NULL;
+	 Next = NULL;
+	 this->Item = Object;
+}
+
+template<typename ZEItemType>
+ZELink<ZEItemType>::~ZELink()
+{
+	zeDebugCheck(List != NULL, "Link has destructed before it it has been removed from the list.");
+}
+
+template<typename ZEItemType>
+ZELink<ZEItemType>* ZEList2<ZEItemType>::GetFirst()
+{
+	return First;
+}
+
+template<typename ZEItemType>
+const ZELink<ZEItemType>* ZEList2<ZEItemType>::GetFirst() const
+{
+	return First;
+}
+
+template<typename ZEItemType>
+ZELink<ZEItemType>* ZEList2<ZEItemType>::GetLast()
+{
+	return Last;
+}
+
+template<typename ZEItemType>
+const ZELink<ZEItemType>* ZEList2<ZEItemType>::GetLast() const
+{
+	return Last;
+}
+
+
+template<typename ZEItemType>
+ZESize ZEList2<ZEItemType>::GetCount() const
+{
+	return Count;
+}
+
+template<typename ZEItemType>
+ZELink<ZEItemType>* ZEList2<ZEItemType>::Find(const ZEItemType* Item, ZELink<ZEItemType>* StartLink)
+{
+	zeDebugCheck(StartLink != NULL && StartLink->List != this, "Start link is not contained in this list.");
+
+	ZELink<ZEItemType>* Cursor = First;
+	if (StartLink != NULL)
+		Cursor = StartLink;
+
+	while(Cursor != NULL)
+	{
+		if (Cursor->GetItem() == Item)
+			return Cursor;
+
+		Cursor = Cursor->Next;
+	}
+
+	return NULL;
+}
+
+template<typename ZEItemType>
+const ZELink<ZEItemType>* ZEList2<ZEItemType>::Find(const ZEItemType* Item, const ZELink<ZEItemType>* StartLink) const
+{
+	zeDebugCheck(StartLink != NULL && StartLink->List != this, "Start link is not contained in this list.");
+
+	const ZELink<ZEItemType>* Cursor = First;
+	if (StartLink != NULL)
+		Cursor = StartLink;
+
+	while(Cursor != NULL)
+	{
+		if (Cursor->GetItem() == Item)
+			return Cursor;
+
+		Cursor = Cursor->Next;
+	}
+
+	return NULL;
+}
+
+template<typename ZEItemType>
+ZESize ZEList2<ZEItemType>::FindIndex(ZELink<ZEItemType>* Link) const
+{
+	ZESize Index = 0;
+	const ZELink<ZEItemType>* Cursor = First;
+	while(Cursor != NULL)
+	{
+		if (Cursor == Link)
+			break;
+
+		Cursor = Cursor->Next;
+		Index++;
+	}
+
+	return Index;
+}
+
+template<typename ZEItemType>
+ZELink<ZEItemType>* ZEList2<ZEItemType>::GetLink(ZESize Index)
+{
+	zeDebugCheck(Index >= Count, "Index parameter is out of range.");
+	ZELink<ZEItemType>* Cursor = First;
+	while(Index != 0)
+	{
+		Cursor = Cursor->Next;
+		Index--;
+	}
+
+	return Cursor;
+}
+
+template<typename ZEItemType>
+const ZELink<ZEItemType>* ZEList2<ZEItemType>::GetLink(ZESize Index) const
+{
+	zeDebugCheck(Index >= Count, "Index parameter is out of range.");
+	const ZELink<ZEItemType>* Cursor = First;
+	while(Index != 0)
+	{
+		Cursor = Cursor->Next;
+		Index--;
+	}
+
+	return Cursor;
+}
+
+template<typename ZEItemType>
+ZEItemType* ZEList2<ZEItemType>::GetItem(ZESize Index)
+{
+	return GetLink(Index)->GetItem();
+}
+
+template<typename ZEItemType>
+void ZEList2<ZEItemType>::AddBegin(ZELink<ZEItemType>* Link)
+{
+	zeDebugCheck(Link == NULL, "Link parameter cannot be null.");
+	zeDebugCheck(Link->List != NULL, "Link already cointained in another list.");
+
+	if (First == NULL)
+	{
+		Link->List = this;
+		First = Link;
+		Last = Link;
+		Count++;
+	}
+	else
+	{
+		InsertBefore(First, Link);
+	}
+}
+
+template<typename ZEItemType>
+void ZEList2<ZEItemType>::AddEnd(ZELink<ZEItemType>* Link)
+{
+	zeDebugCheck(Link == NULL, "Link parameter cannot be null.");
+	zeDebugCheck(Link->List != NULL, "Link already cointained in another list.");
+
+	if (Last == NULL)
+	{
+		Link->List = this;
+		First = Link;
+		Last = Link;
+		Count++;
+	}
+	else
+	{
+		InsertAfter(Last, Link);
+	}
+}
+
+template<typename ZEItemType>
+void ZEList2<ZEItemType>::InsertBefore(ZELink<ZEItemType>* Link, ZELink<ZEItemType>* NewLink)
+{
+	zeDebugCheck(Link->List != this, "Link is not in this list.");
+	zeDebugCheck(Link == NULL, "Link parameter cannot be null.");
+	zeDebugCheck(NewLink == NULL, "NewLink parameter cannot be null.");
+	zeDebugCheck(NewLink->List != NULL, "NewLink already cointained in another list.");
+
+	NewLink->Prev = Link->Prev;
+	NewLink->Next = Link;
+	NewLink->List = this;
+
+	if (Link->Prev == NULL)
+		First = NewLink;
+	else
+		Link->Prev->Next = NewLink;
+
+	Link->Prev = NewLink;
+
+	Count++;
+}
+
+template<typename ZEItemType>
+void ZEList2<ZEItemType>::InsertAfter(ZELink<ZEItemType>* Link, ZELink<ZEItemType>* NewLink)
+{
+	zeDebugCheck(Link->List != this, "Link is not in this list.");
+	zeDebugCheck(Link == NULL, "Link parameter cannot be null.");
+	zeDebugCheck(NewLink == NULL, "NewLink parameter cannot be null.");
+	zeDebugCheck(NewLink->List != NULL, "NewLink already cointained in another list.");
+
+	NewLink->Prev = Link;
+	NewLink->Next = Link->Next;
+	NewLink->List = this;
+
+	if (Link->Next == NULL)
+		Last = NewLink;
+	else
+		Link->Next->Prev = NewLink;
+
+	Link->Next = NewLink;
+
+	Count++;
+}
+
+template<typename ZEItemType>
+void ZEList2<ZEItemType>::Remove(ZELink<ZEItemType>* Link)
+{
+	zeDebugCheck(Link == NULL, "Link parameter cannot be null.");
+	zeDebugCheck(Link->List != this, "Link is not in this list.");
+
+	if (Link->Prev == NULL)
+		First = Link->Next;
+	else
+		Link->Prev->Next = Link->Next;
+
+	if (Link->Next == NULL)
+		Last = Link->Prev;
+	else
+		Link->Next->Prev = Link->Prev;
+
+	Link->List = NULL;
+	Link->Prev = NULL;
+	Link->Next = NULL;
+
+	Count--;
+}
+
+template<typename ZEItemType>
+void ZEList2<ZEItemType>::Swap(ZELink<ZEItemType>* A, ZELink<ZEItemType>* B)
+{
+	zeDebugCheck(A == NULL, "A parameter cannot be null.");
+	zeDebugCheck(A->List != this, "A is not in this list.");
+
+	zeDebugCheck(B == NULL, "B parameter cannot be null.");
+	zeDebugCheck(B->List != this, "B is not in this list.");
+
+	ZELink<ZEItemType>* Temp;
+	Temp = A->Prev;
+	A->Prev = B->Prev;
+	B->Prev = Temp;
+
+	Temp = A->Next;
+	A->Next = B->Next;
+	B->Next = Temp;
+
+	if (First == A)
+		First = B;
+	else if (First == B)
+		First = A;
+
+	if (Last == A)
+		Last = B;
+	else if (Last == B)
+		Last = A;
+}
+
+
+template<typename ZEItemType>
+inline void ZEList2<ZEItemType>::Clean()
+{
+	ZELink<ZEItemType>* Link = First;
+	while(Link != NULL)
+	{
+		ZELink<ZEItemType>* Temp = Link->Next;
+		Link->Prev = NULL;
+		Link->Next = NULL;
+		Link->List = NULL;
+		Link = Temp;
+	}
+	Count = 0;
+}
+
+template<typename ZEItemType>
+void ZEList2<ZEItemType>::Reverse()
+{
+	ZELink<ZEItemType>* Temp;
+
+	ZELink<ZEItemType>* Link = First;
+	while(Link != NULL)
+	{
+		Temp = Link->Prev;
+		Link->Prev = Link->Next;
+		Link->Next = Temp;
+
+		Link = Link->Prev;
+	}
+
+	Temp = First;
+	First = Last;
+	Last = Temp;
+}
+
+template<typename ZEItemType>
+ZEItemType* ZEList2<ZEItemType>::operator[](ZESize Index) const
+{
+	return GetItem(Index);
+}
+
+template<typename ZEItemType>
+ZEList2<ZEItemType>::ZEList2()
+{
+	First = NULL;
+	Last = NULL;
+	Count = 0;
+}
+
+template<typename ZEItemType>
+ZEList2<ZEItemType>::~ZEList2()
+{
+	Clean();
+}
+
+template<typename ZEItemType>
+template<ZEInt CompareFunction(const ZEItemType*, const ZEItemType*)>
+void ZEList2<ZEItemType>::Sort()
+{
+	// Source: ideasman42/linked_list_sort.c - https://gist.github.com/ideasman42/5921b0edfc6aa41a9ce0
+
+	int BlockSize = 1, BlockCount;
+	do 
+	{
+		ZELink<ZEItemType>* Left = First;
+		ZELink<ZEItemType>* Right = First;
+
+		First = NULL;
+		Last = First;
+
+		BlockCount = 0;
+		while (Left != NULL) 
+		{
+			ZESize LeftSize;
+			ZESize RightSize = BlockSize;
+
+			BlockCount++;
+			for (LeftSize = 0; (LeftSize < BlockSize) && (Right != NULL); LeftSize++) 
+				Right = Right->Next;
+
+			bool LeftEmpty = (LeftSize == 0);
+			bool RightEmpty = (RightSize == 0 || Right == NULL);
+			while (!LeftEmpty || !RightEmpty) 
+			{
+				ZELink<ZEItemType>* Selected;
+				if (RightEmpty || (!LeftEmpty && CompareFunction(Left->GetItem(), Right->GetItem()) <= 0)) 
+				{
+					Selected = Left;
+					Left = Left->Next;
+					LeftSize--;
+					LeftEmpty = (LeftSize == 0);
+				}
+				else 
+				{
+					Selected = Right;
+					Right = Right->Next;
+					RightSize--;
+					RightEmpty = (RightSize == 0) || (Right == NULL);
+				}
+
+				if (Last != NULL)
+					Last->Next = Selected;
+				else
+					First = Selected;
+				Selected->Prev = Last;
+				Last = Selected;
+			}
+
+			Left = Right;
+		}
+
+		if (Last != NULL)
+			Last->Next = NULL;
+
+		BlockSize <<= 1;
+	} 
+	while (BlockCount > 1);
+}
